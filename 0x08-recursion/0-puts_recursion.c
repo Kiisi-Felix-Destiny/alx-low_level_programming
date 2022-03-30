10c913d99651564861f6eefaaf5ce0e2733e34ab
Kiisi-Felix-Destiny <kiisifelix06@gmail.com>
2022-03-30 16:39:37 -0700

    Done

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,13 +7,12 @@
 
 void _puts_recursion(char *s)
 {
-	int i;
-
-	i = 0;
-
-	for (; s[i] != '\0'; i++)
+	if (*s != '\0')
 	{
-		_putchar(s[i]);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
+
 }
