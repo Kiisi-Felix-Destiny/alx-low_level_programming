91c9215fc36fede2911d2e17459adf1e02a4918a
Kiisi-Felix-Destiny <kiisifelix06@gmail.com>
2022-04-01 14:28:34 -0700

    Done

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
-/**/**
- * @brief 
- * 
- */
+/**
+ * main - Entry point
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc - 1);
+	int count, i = 0;
+
+	if (argc > 1)
+	{
+		for (count = 1; count < argc; count++)
+		{
+			i++;
+		}
+		printf("%d\n", i);
+	}
+	else
+		printf("0\n");
+
 	return (0);
 }
