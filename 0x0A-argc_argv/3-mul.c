f92d70469348cfe4d165ea0b39f68103de6ecf7e
Kiisi-Felix-Destiny <kiisifelix06@gmail.com>
2022-04-01 12:24:54 -0700

    Done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: number of argv array
+ * @argv: an array of command line arguements
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int count, mul;
+
+	if (argc > 1)
+	{
+		for (count = 0; count < argc; count++)
+		{
+			mul = atoi(argv[1]) * atoi(argv[2]);
+		}
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error");
+		printf("\n");
+	}
+	return (0);
+}
