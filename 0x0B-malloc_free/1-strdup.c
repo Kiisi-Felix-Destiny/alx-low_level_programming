e52a5339c4f9dad8515175612d4d4e4e2bf35458
Kiisi-Felix-Destiny <kiisifelix06@gmail.com>
2022-04-04 06:20:30 -0700

    Done

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,68 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - count array
+ * @s: array of elements
+ * Return: i
+ */
+
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * _strcpy - copy arrays
+ * @src: array of elements
+ * @dest: dest array
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * _strup - array for prints a string
+ * @str: array of elements
+ * Return: pointer
+ */
+char *_strup(char *str)
+{
+	char *dest;
+
+	unsigned int size;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+	size = _strlen(size) + 1;
+
+	dst = (char *) malloc(size * sizeof(char));
+
+	if (dst == 0)
+	{
+		return (NULL);
+	}
+	_strcpy(dst, str);
+	return (dst);
+}
