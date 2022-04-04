2e8ba9ad72110688bc0c1f6665040e3d5cbdd57a
Kiisi-Felix-Destiny <kiisifelix06@gmail.com>
2022-04-04 05:51:09 -0700

    Done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	buffer = (int *) malloc(size * sizeof(c));
+	buffer = (char *) malloc(size * sizeof(c));
 
 	if (buffer == 0)
 	{
