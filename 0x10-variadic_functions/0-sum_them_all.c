edc9bf43476f0527ac241c89bd7d7535643e4dd4
Kiisi-Felix-Destiny <kiisifelix06@gmail.com>
2022-04-14 14:13:46 -0700

    Done

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum all the arguments
+ * @n: first paramter of the va_list
+ *
+ * Return: sum of its parameter
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list valist;
+
+	unsigned int i;
+
+	int sum;
+	
+	sum = 0;
+
+	if (n == 0)
+		return (0);
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+
+	va_end(valist);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef _SUM_THEM_ALL_
+#define _SUM_THEM_ALL_
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
