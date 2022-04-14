2b15679f4741eedd0b1311e36682d6f69d49bcc4
Kiisi-Felix-Destiny <kiisifelix06@gmail.com>
2022-04-14 14:51:53 -0700

    Done

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,8 @@
-#ifndef _SUM_THEM_ALL_
-#define _SUM_THEM_ALL_
+#ifndef _VARIADIC_FUNCTIONS_
+#define _VARIADIC_FUNCTIONS_
+
+#include <stdio.h>
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
